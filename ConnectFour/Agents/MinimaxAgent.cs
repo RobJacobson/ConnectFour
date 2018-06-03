a33ae6a62064c158d49e54de9df42e8b00646f64
Robert Jacobson <rljacobson@ymail.com>
2018-06-03 11:08:00 -0700

    Refactor

--- ConnectFour/Agents/MinimaxAgent.cs
+++ ConnectFour/Agents/MinimaxAgent.cs
@@ -9,16 +9,26 @@ namespace ConnectFour.Agents
 {
     abstract class MinimaxAgent : Agent
     {
-        public int TotalIterations { get; }
+        private int minmaxIterations = 0;
+
+        // Override base constructor
+        public MinimaxAgent(Color player) : base(player) { }
 
         public override int GetNextMoveDerived(Board board)
         {
+            minmaxIterations++;
             throw new NotImplementedException();
         }
 
+
+        // Unique implementation of heuristic for each derived MinMax agent
+        public abstract int Heuristic(Board board);
+
+
+        // Show the total number of MinMax iterations for each derived agent
         public override string ToString()
         {
-            return base.ToString() + $", Iterations: " + TotalIterations;
+            return base.ToString() + $", Iterations: " + minmaxIterations;
         }
     }
 
